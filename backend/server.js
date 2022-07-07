27685795202c4f70c9f84f7f8c255c75541d164a
Carlos Muñoz <camr.pesonal@gmail.com>
2022-07-06 19:16:13 -0500

    Lesson 3: Express Router & API Routes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const workoutRouter = require('./routes/workout');
 
 // express app
 const app = express();
 
 // middleware
+app.use(express.json())
+
 app.use( (req,res,next) => {
     console.log(req.path, req.method);
     next();
 })
 
 // routes
-app.get('/', (req,res) => {
-    res.json({mssg: 'Welcome to the backend.'});
-})
+app.use('/api/workout', workoutRouter);
 
 // listening for requests on port 4000
 app.listen(4000, () => {
