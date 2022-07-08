ca15161a283d191c079b39584fc7b06c28ab7216
Carlos Muñoz <camr.pesonal@gmail.com>
2022-07-08 13:53:02 -0500

    Lesson 6: Controllers (part 1)

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,57 @@
+const Workout = require('../models/workoutModel');
+const mongoose = require('mongoose');
+
+// GET all workouts
+const getWorkouts = async (req,res) => {
+    try {
+        const workout = await Workout.find({}).sort({createdAt: -1});
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+};
+
+// GET a single workout
+const getWorkout = async (req,res) => {
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such workout.'});
+    };
+
+    const workout = await Workout.findById(id);
+
+    if(!workout){
+        return res.status(404).json({error:'No such workout.'});
+    };
+
+    res.status(200).json(workout);
+};
+
+// CREATE a new workout
+const createWorkout = async (req,res) => {
+    const {title,reps,load} = req.body;
+
+    // add a doc to database
+    try {
+        const workout = await Workout.create({title,reps,load});
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+
+
+// DELETE a workout
+
+
+
+// UPDATE a workout
+
+
+module.exports = {
+    createWorkout,
+    getWorkouts,
+    getWorkout
+};
