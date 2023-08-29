1c61e0e66a176b6811ed9cad0abf5b1c839879e3
Carlos Muñoz <camr.pesonal@gmail.com>
2023-08-29 16:59:53 -0500

    user backend

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,32 @@
+const User = require("../models/userSchema")
+const jwt = require("jsonwebtoken")
+
+const createToken = (_id) =>{
+  return jwt.sign({_id}, process.env.SECRET,{expiresIn: "10m"})
+}
+
+const userSignup = async (req,res) =>{
+  const {email,password} = req.body
+
+  try {
+    const user = await User.signup(email,password)
+    const token = createToken(user._id)
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+const userLogin = async (req,res) =>{
+  const {email,password} = req.body
+
+  try {
+    const user = await User.login(email,password)
+    const token = createToken(user._id)
+    res.status(200).json({email,token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+module.exports = {userSignup, userLogin}
---
+++ backend/models/userSchema.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const validator = require("validator")
+const bcrypt = require("bcrypt")
+
+const userSchema = new Schema({
+  email:{
+    type: String,
+    required: true,
+    unique: true
+  },
+  password:{
+    type: String,
+    required: true,
+  }
+},{timestamps: true})
+
+
+userSchema.statics.signup = async function(email,password){
+  if(!email || !password){
+    throw Error("all the fields must be fill in.")
+  }
+
+  if(!validator.isEmail(email)){
+    throw Error("you must use a valid email.")
+  }
+
+  if(!validator.isStrongPassword(password)){
+    throw Error("please, use a strong password.")
+  }
+
+  const exist = await this.findOne({email})
+
+  if(exist){
+    throw Error("this is email is already in use.")
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+  const user = await this.create({email,password: hash})
+
+  return user
+}
+
+userSchema.statics.login = async function(email,password){
+  if(!email || !password){
+    throw Error("all the fields must be fill in.")
+  }
+
+  if(!validator.isEmail(email)){
+    throw Error("you must use a valid email.")
+  }
+
+  const user = await this.findOne({email})
+
+  if(!user){
+    throw Error("this user doesn't exist.")
+  }
+
+  const validPass = await bcrypt.compare(password, user.password)
+  
+  if(!validPass){
+    throw Error("this is not a valid password.")
+  }
+
+  return user
+}
+
+
+module.exports = mongoose.model("User", userSchema)
